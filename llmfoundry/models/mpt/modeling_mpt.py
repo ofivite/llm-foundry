90b25a82c014bb33885f8c9ae664a9dc564bbb0c
ofivite <20295221+ofivite@users.noreply.github.com>
2025-02-07 11:40:36 +0100

    fixing init of unembedding

--- llmfoundry/models/mpt/modeling_mpt.py
+++ llmfoundry/models/mpt/modeling_mpt.py
@@ -22,6 +22,7 @@ from typing import (
 
 import torch
 import torch.nn as nn
+import torch.nn.init as init
 import torch.nn.functional as F
 from composer.models import HuggingFaceModel
 from composer.utils import dist
@@ -1059,6 +1060,9 @@ class MPTForCausalLM(MPTPreTrainedModel):
                 bias=False,
                 device=config.init_device,
             )
+            unemb_std = (config.vocab_size / config.d_model)**0.5 / (config.d_model**0.5 + config.vocab_size**0.5)
+            init.normal_(self.lm_head.weight, mean=0.0, std=unemb_std) #  * (config.d_model / 256)**0.5
+
             self.lm_head._fsdp_wrap = True
             self.lm_head._is_unembedding = True
 
